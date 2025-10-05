780abc92802d646670dd61f14ec9edd3f3661e49
dhofmann4663-maker <dhofmann4663@gmail.com>
2025-10-05 18:57:04 -0500

    adding analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { Analytics } from '@vercel/analytics/react'
+import { Analytics } from '@vercel/analytics/react';
+ 
+export default function App() {
+  return (
+    <div>
+      {/* ... */}
+      <Analytics />
+    </div>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
